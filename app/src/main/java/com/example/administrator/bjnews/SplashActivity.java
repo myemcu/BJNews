a18d93b95614de9ad6173789b09f085e02375323
myemcu <1142497680@qq.com>
2016-09-05 16:45:22 +0800

    V2，监听动画播放完成。

--- app/src/main/java/com/example/administrator/bjnews/SplashActivity.java
+++ app/src/main/java/com/example/administrator/bjnews/SplashActivity.java
@@ -8,6 +8,7 @@ import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class SplashActivity extends Activity {
 
@@ -22,21 +23,17 @@ public class SplashActivity extends Activity {
 
         // 1 缩放动画                           0:很小，1：完整，相对于自身
         ScaleAnimation sa = new ScaleAnimation(0,1,0,1,ScaleAnimation.RELATIVE_TO_SELF,0.5f,ScaleAnimation.RELATIVE_TO_SELF,0.5f);  // Ctrl+P显示括号中的参数，选最长的
-        sa.setDuration(2000);   // 动画持续2s
-        sa.setFillAfter(true);  // 停留在播放后的状态
 
         // 2 渐变动画
         AlphaAnimation aa = new AlphaAnimation(0,1);
-        aa.setDuration(2000);
-        aa.setFillAfter(true);
 
         // 3 旋转动画
         RotateAnimation ra = new RotateAnimation(0,360,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        ra.setDuration(2000);
-        ra.setFillAfter(true);
 
         // 同时播放3个动画
         AnimationSet set = new AnimationSet(false); // false:插入器
+        set.setDuration(2000);                      // 延时
+        set.setFillAfter(true);                     // 停留在播放后的状态
         set.addAnimation(sa);
         set.addAnimation(aa);
         set.addAnimation(ra);
@@ -44,5 +41,32 @@ public class SplashActivity extends Activity {
         // 实例化相对布局对象
         rl_splash_root = (RelativeLayout) findViewById(R.id.rl_splash_root);
         rl_splash_root.startAnimation(set);
+
+        //------------------------------------------------------------------------------------------
+
+        // 监听动画播放完成，监听对象：set
+        set.setAnimationListener(new MyAnimationListener());
+    }
+
+    class MyAnimationListener implements Animation.AnimationListener{
+
+        /*当动画开始播放的时候回调这个方法*/
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        /*当动画播放完成的时候回调这方法*/
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            Toast.makeText(SplashActivity.this,"动画播放完成",Toast.LENGTH_LONG).show();
+        }
+
+        /*当动画重复播放的时候回调这个方法*/
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
