986ee6cc57ea20b5ae7a1c58a1aa703256cc305f
myemcu <1142497680@qq.com>
2016-09-09 17:14:22 +0800

    V11，使用xUtils3的注解功能实现控件实例化并初始选中“首页”。

--- app/src/main/java/com/example/administrator/bjnews/base/fragment/ContentFragment.java
+++ app/src/main/java/com/example/administrator/bjnews/base/fragment/ContentFragment.java
@@ -4,10 +4,15 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.administrator.bjnews.R;
 import com.example.administrator.bjnews.base.BaseFragment;
+
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
 
 // 说明：新类继承父类，就是要实现父类中的相关方法，其中抽象方法必须实现，其它方法根据需要选择实现。
 
@@ -17,8 +22,14 @@ import com.example.administrator.bjnews.base.BaseFragment;
  */
 public class ContentFragment extends BaseFragment{
 
+    @ViewInject(R.id.fl_content)
+    private FrameLayout fl_content;
+
+    @ViewInject(R.id.rg_bottom_tag)
+    private RadioGroup rg_bottom_tag;
+
     private static final String TAG = ContentFragment.class.getSimpleName();
-    private TextView txt;
+//    private TextView txt;
 
     @Override
     public View initView() {
@@ -28,7 +39,11 @@ public class ContentFragment extends BaseFragment{
         txt.setTextColor(Color.RED);
         return txt;*/
 
-        View view = View.inflate(context,R.layout.content_fragment,null);//inflate将一个xml中定义的布局控件找出来
+        View view = View.inflate(context,R.layout.content_fragment,null);   // inflate将一个xml中定义的布局控件找出来
+
+        //使用xUtils3实现注解，以方便实例化
+        x.view().inject(this,view); // this:ContentFragment.java，view:content_fragment.xml(把当前XML中的视图注入到当前的java中)
+
         return view;
     }
 
@@ -37,5 +52,6 @@ public class ContentFragment extends BaseFragment{
         super.initData();
         Log.i(TAG,"主页数据初始化完成..");
         //txt.setText("主页内容..\nFragment");
+        rg_bottom_tag.check(R.id.rb_home);
     }
 }
