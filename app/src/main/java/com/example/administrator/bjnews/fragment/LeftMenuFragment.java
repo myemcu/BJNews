b9248e162db25542a99e8a4921812cf313d8d136
myemcu <1142497680@qq.com>
2016-09-20 11:35:20 +0800

    V22，构建左侧菜单列表项。

--- app/src/main/java/com/example/administrator/bjnews/fragment/LeftMenuFragment.java
+++ app/src/main/java/com/example/administrator/bjnews/fragment/LeftMenuFragment.java
@@ -4,10 +4,16 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.administrator.bjnews.R;
 import com.example.administrator.bjnews.base.BaseFragment;
 import com.example.administrator.bjnews.bean.NewsCenterBean;
+import com.example.administrator.bjnews.utils.DensityUtil;
 
 import java.util.List;
 
@@ -19,29 +25,58 @@ import java.util.List;
 public class LeftMenuFragment extends BaseFragment{
 
     private static final String TAG = LeftMenuFragment.class.getSimpleName();
-    private TextView txt;
     private List<NewsCenterBean.DataBean> leftMenuData; // 左侧菜单的数据
+    private ListView listView;
 
     @Override
     public View initView() {
-        txt = new TextView(context);
-        txt.setGravity(Gravity.CENTER);
-        txt.setTextSize(25);
-        txt.setTextColor(Color.RED);
-        return txt;
+        listView = new ListView(context);                           // 以Java方式(动态)创建ListView
+        listView.setBackgroundColor(Color.BLACK);                   // 背景黑色
+        listView.setPadding(0, DensityUtil.dip2px(context,40),0,0); // 顶边距40
+        listView.setDividerHeight(0);                               // 分隔线高
+        listView.setCacheColorHint(Color.TRANSPARENT);              // 设置透明(防止在低版本中的按下变色)
+        listView.setSelector(android.R.color.transparent);          // 选择器透明(当选择某条的时候，无颜色变化)
+
+
+        return listView;
     }
 
     @Override
     public void initData() {
         super.initData();
         Log.i(TAG,"侧滑菜单数据初始化完成..");
-        txt.setText("侧滑菜单..\nFragment");
     }
 
     public void setData(List<NewsCenterBean.DataBean> Data) {
-        this.leftMenuData=Data; // 将从形参中传递进来的数据赋值到本Fragment中的对象leftMenuData
+        this.leftMenuData=Data;     // 将从形参中传递进来的数据赋值到本Fragment中的对象leftMenuData
         for (int i=0; i<leftMenuData.size(); i++) {
             System.out.println(leftMenuData.get(i).getTitle()+"-----------");
+        }
+        listView.setAdapter(new LeftMenuAdapter());
+    }
+
+    private class LeftMenuAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return leftMenuData.size();         // 返回列表项条目数
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            TextView txt = (TextView) View.inflate(context, R.layout.item_leftmenu,null);  // 文本几乎不耗资源，故不作优化
+
+            return txt;
         }
     }
 }
--- app/src/main/java/com/example/administrator/bjnews/utils/Url.java
+++ app/src/main/java/com/example/administrator/bjnews/utils/Url.java
@@ -2,11 +2,11 @@ package com.example.administrator.bjnews.utils;
 
 /**
  * Created by Administrator on 2016/9/14 0014.
- * http://192.168.1.100:8080/zhbj/categories.json
+ * http://192.168.1.109:8080/zhbj/categories.json
  * 配置联网请求链接
  */
 public class Url {
     /*新闻中心的联网链接*/
-    public static String BASE_URL= "http://192.168.1.100:8080/zhbj";
+    public static String BASE_URL= "http://192.168.1.109:8080/zhbj";
     public static final String NEWSCENTER_URL = BASE_URL+"/categories.json";
 }
