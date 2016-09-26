2f714fa13f29a52b5514cb1975fb596fcf0f5bde
myemcu <1142497680@qq.com>
2016-09-26 16:01:49 +0800

    V34，中框（页签详情页面）视图与数据。

--- app/src/main/java/com/example/administrator/bjnews/menudetail/TabDetailPager.java
+++ app/src/main/java/com/example/administrator/bjnews/menudetail/TabDetailPager.java
@@ -6,18 +6,22 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.administrator.bjnews.R;
 import com.example.administrator.bjnews.base.MenuDetailBasePager;
 import com.example.administrator.bjnews.bean.NewsCenterBean_Hand;
+import com.example.administrator.bjnews.utils.Url;
 
 /**
  * Created by Administrator on 2016/9/23 0023.
- * 页签页面
+ * 页签详情页面
  */
 
 public class TabDetailPager extends MenuDetailBasePager {
 
+    private String url;
+
     private final NewsCenterBean_Hand.NewsCenterBean_Data.Children_Data childrenDatas;
-    private TextView txt;
+//    private TextView txt;
 
     // 构造器
     public TabDetailPager(Context context, NewsCenterBean_Hand.NewsCenterBean_Data.Children_Data data) {
@@ -27,17 +31,17 @@ public class TabDetailPager extends MenuDetailBasePager {
 
     @Override
     public View initView() {    // 视图
-        txt = new TextView(context);
-        txt.setGravity(Gravity.CENTER);
-        txt.setTextSize(25);
-        txt.setTextColor(Color.BLUE);
-        return txt;
+
+        View view = View.inflate(context, R.layout.tabdetail_pager,null);
+
+        return view;
     }
 
     @Override
     public void initData() {    // 数据
         super.initData();
         System.out.println("---------"+childrenDatas.getTitle());
-        txt.setText(childrenDatas.getTitle());
+        url= Url.BASE_URL+childrenDatas.getUrl();
+        System.out.println("url==="+url);
     }
 }
