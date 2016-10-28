0df801b6379b5cc04eab5089c751dec52f1a9392
myemcu <1142497680@qq.com>
2016-10-28 10:52:43 +0800

    V59，更改组图UI-Style。

--- app/src/main/java/com/example/administrator/bjnews/utils/Url.java
+++ app/src/main/java/com/example/administrator/bjnews/utils/Url.java
@@ -10,10 +10,13 @@ public class Url {
     /*Tomacat基站服务包*/
     public static String BASE_URL= "http://192.168.1.200:8080/zhbj";
 
+
     /*新闻路径*/
     public static final String NEWSCENTER_URL = BASE_URL+"/categories.json";
 
-    /*组图路径*/
+
+    /*组图路径验证*/
     /*http://192.168.1.200:8080/zhbj/photos/photos_1.json*/
     public static final String PHOTOS_URL = BASE_URL+"/photos/photos_1.json";
+
 }
