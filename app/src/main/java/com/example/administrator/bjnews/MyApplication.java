53a0e7cb07a0514c602e8fe9212c9d44b4e98905
myemcu <1142497680@qq.com>
2016-10-31 16:07:20 +0800

    V64，集成极光推送。

--- app/src/main/java/com/example/administrator/bjnews/MyApplication.java
+++ app/src/main/java/com/example/administrator/bjnews/MyApplication.java
@@ -6,6 +6,8 @@ import com.example.administrator.bjnews.volley.VolleyManager;
 
 import org.xutils.x;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by Administrator on 2016/9/12 0012.
  */
@@ -18,5 +20,8 @@ public class MyApplication extends Application{
         x.Ext.setDebug(true);
 
         VolleyManager.init(this);
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
